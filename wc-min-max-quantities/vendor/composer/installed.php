6db05b53291864cd5cd6939e7554460a45b4f3b4
arman-ario <dgshop00@gmail.com>
2025-09-16 16:52:36 +0330

    Add files via upload

---
+++ wc-min-max-quantities/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pluginever/wc-min-max-quantities',
+        'pretty_version' => 'v2.1.8',
+        'version' => '2.1.8.0',
+        'reference' => '81302e1525fbea172aed024089b1ddbb5980c14c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'byteever/bytekit-plugin' => array(
+            'pretty_version' => 'v1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => '794ec9834ecac6cead6b1064a605ff930864950b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../byteever/bytekit-plugin',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'byteever/bytekit-settings' => array(
+            'pretty_version' => 'v1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => '6fed19fd393c2037b967d5507d26cb98a1474bbd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../byteever/bytekit-settings',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pluginever/wc-min-max-quantities' => array(
+            'pretty_version' => 'v2.1.8',
+            'version' => '2.1.8.0',
+            'reference' => '81302e1525fbea172aed024089b1ddbb5980c14c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
